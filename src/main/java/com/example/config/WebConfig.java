52475eef61b962c8280e6a2c1211afeebb75d257
WillyLu326 <luzh0326@gmail.com>
2017-09-04 23:21:45 -0400

    webConfig useless

--- src/main/java/com/example/config/WebConfig.java
+++ src/main/java/com/example/config/WebConfig.java
@@ -7,14 +7,12 @@ import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 import com.fasterxml.jackson.databind.SerializationFeature;
 
 @Configuration
-@ComponentScan({ "com.example.config" })
-@EnableWebMvc
+@ComponentScan(basePackages={ "com.example.config" })
 public class WebConfig extends WebMvcConfigurerAdapter{
 
 	@Override
