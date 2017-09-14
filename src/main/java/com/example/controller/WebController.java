e0de76e4745f5733965caf982bc01951c7c59704
WillyLu326 <luzh0326@gmail.com>
2017-09-13 22:06:29 -0400

    create webcontroller

---
+++ src/main/java/com/example/controller/WebController.java
@@ -0,0 +1,29 @@
+package com.example.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.exception.CustomNotFoundException;
+import com.example.model.Customer;
+import com.example.services.CustomerService;
+
+@RestController
+public class WebController {
+
+	@Autowired
+	CustomerService consumerService;
+	
+	@RequestMapping(value = "/customer/{name}", method = RequestMethod.GET)
+	public Customer findCustomerByName(@PathVariable("name") String name) {
+		Customer customer = this.consumerService.findCustomerByName(name);
+		
+		if (customer == null) {
+			throw new CustomNotFoundException("Not found customer with name " + name);
+		}
+		
+		return customer;
+	}
+}
