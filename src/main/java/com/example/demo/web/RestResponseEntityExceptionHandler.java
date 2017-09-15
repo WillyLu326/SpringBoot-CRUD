a4d22bc73467d25666629475dbbbf9d0f926f2cc
WillyLu326 <luzh0326@gmail.com>
2017-09-14 21:14:01 -0400

    create RestResponseEntityExceptionHandler

---
+++ src/main/java/com/example/demo/web/RestResponseEntityExceptionHandler.java
@@ -0,0 +1,10 @@
+package com.example.demo.web;
+
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
+
+	public RestResponseEntityExceptionHandler() {
+		super();
+	}
+}
