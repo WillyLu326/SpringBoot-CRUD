d1d7362dfc9e1967af0259cc360d1035bef10401
WillyLu326 <luzh0326@gmail.com>
2017-09-14 21:27:31 -0400

    overrid handleMethodArgumentNotValid method

--- src/main/java/com/example/demo/web/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/web/RestResponseEntityExceptionHandler.java
@@ -4,6 +4,7 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -16,8 +17,14 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 	@Override
 	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
 			HttpHeaders headers, HttpStatus status, WebRequest request) {
-		// TODO Auto-generated method stub
 		return handleExceptionInternal(ex, "There is a problem", headers, HttpStatus.BAD_REQUEST, request);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleMethodArgumentNotValid(MethodArgumentNotValidException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		
+		return handleExceptionInternal(ex, "There is a bad request", headers, status, request);
 	}
 	
 	
