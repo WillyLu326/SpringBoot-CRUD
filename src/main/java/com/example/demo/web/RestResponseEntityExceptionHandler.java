40c45b42b98e4ad87142c5af945fc8a183d6c92a
WillyLu326 <luzh0326@gmail.com>
2017-09-14 21:17:32 -0400

    override handleHttpMessageNotReadable method

--- src/main/java/com/example/demo/web/RestResponseEntityExceptionHandler.java
+++ src/main/java/com/example/demo/web/RestResponseEntityExceptionHandler.java
@@ -1,5 +1,10 @@
 package com.example.demo.web;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionHandler {
@@ -7,4 +12,13 @@ public class RestResponseEntityExceptionHandler extends ResponseEntityExceptionH
 	public RestResponseEntityExceptionHandler() {
 		super();
 	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(HttpMessageNotReadableException ex,
+			HttpHeaders headers, HttpStatus status, WebRequest request) {
+		// TODO Auto-generated method stub
+		return handleExceptionInternal(ex, "There is a problem", headers, HttpStatus.BAD_REQUEST, request);
+	}
+	
+	
 }
